f641575836638859b9857e52be5de0814d61a509
Kashfia <kashfiabintae@gmail.com>
2024-09-24 18:55:15 +0600

    first commit

---
+++ List.py
@@ -0,0 +1,13 @@
+list1 = ["kashfia", "Junaira", "Rifa"]
+print(list1)
+
+print(list1[0])
+print(list1[1])
+print(list1[2])
+
+list1.append("Giga")
+print(list1)
+print(list1[3])
+
+aList = [100, 200, 300, 400, 500]
+print( aList[::-1] )
